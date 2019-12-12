b81d5e89f1d1aff5ccfe99714bc1734eb496b605
christopher tate <chris29tate2@hotmail.co.uk>
2019-12-12 23:44:17 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,5 +1,5 @@
 def unsafe?(speed)
-return  true if speed is greater speed = 60 
+return  true if speed is > speed = 60 
 return false if speed is between = 40 & 50
 answer = 35 
 end
@@ -7,9 +7,9 @@ end
 
 
 def not_safe?(speed)
-  return true if speed is greater than 60
+  return true if speed is > than 60
   return false if speed is between 40 & 60
-  return if the speed is less than 40 
+  return if the speed is < than 40 
 	answer = 79
 end
 	
